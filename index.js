9dfaee5df0e5fd331ad3a67fd4621dff597cf9a4
Sibo_Fulgencio <eduardo.pacheco@utexas.edu>
2022-08-31 22:49:20 -0500

    Finished the Initialize function - Created function that generates the HTML file

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const inquirer = require("inquirer");
+const fs = require('fs');
+const path = require('path');
+
 const questionsDB = require("./src/questions.js")
+const classes = require("./lib/classes")
+const template = require("./src/template")
+
+
+function writeToFile(fileName, data) {
+    fs.writeFile(__dirname+'/dist/'+fileName, data, (err) =>
+    err ? console.log(err) : console.log('Team Successfully created!')
+  );
+}
 
 
 const addTeamMember = async (answerCollector = []) => {
     const { addAnother, ...currentAnswers } = await inquirer.prompt(questionsDB.internOrEngineer)
-    const answerArray = [...answerCollector, currentAnswers]
-    return addAnother == 'Yes' ? addTeamMember(answerArray) : answerArray;        
+    answerCollector = [...answerCollector, currentAnswers]
+    return addAnother == 'Yes' ? addTeamMember(answerCollector) : answerCollector;        
 }
 
 const managerQuestions = async () => {
@@ -23,7 +35,12 @@ const initailize = async (results = []) => {
     const membersResults = await addTeamMember()
     membersResults.push(managerResults)
 
-    console.log('sssss',membersResults)
+    // console.log('sssss',membersResults)
+
+    classes.buildTeam(membersResults)
+
+    const htmlText = template.fillCards()
+    writeToFile('techTeam.html', htmlText)
         
 }
 
